4cab873cedb5dc8c2ac340d621f56a8c46233aba
Charlie-Palmer <charlesezra96@gmail.com>
2025-01-06 11:45:33 +0000

    recent edits

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,30 +1,46 @@
 import pandas as pd
 import numpy as np
-
+from dateutil import parser
 
 
 
 class DataCleaning:
     
     #Changes dates to datetime, any data that cannot be converted to datetime will be converted to NaT values
-    def clean_invalid_dates(self, df, column_name):
-        df['column_name'] = pd.to_datetime(column_name, format='%B-%y-%d', errors='coerce')
+    def clean_invalid_dates(self, df: pd.DataFrame, column_name):
+        def parse_date(value):
+            try:
+                # Ensure the value is a string that looks like a date
+                if isinstance(value, str) and any(char.isdigit() for char in value):
+                    return parser.parse(value)
+                else:
+                    return pd.NaT
+            except (ValueError, TypeError, parser.ParserError):
+                return pd.NaT
+
+        # Step 1: Apply the parse_date function to the column
+        df[column_name] = df[column_name].apply(parse_date)
+
+        # Step 2: Format valid dates to the desired string format
+        df[column_name] = df[column_name].apply(
+            lambda x: x.strftime("%d-%m-%Y") if pd.notnull(x) else pd.NaT
+        )       
         return df
-    
+        
     def remove_null(self, df: pd.DataFrame):
-        df = df.replace("NULL", pd.NA, inplace=True)
+        df = df.replace("NULL", pd.NA)        
         return df    
+    
     #Performs cleaning by removing null values and formatting the join date to datetime.
     def clean_user_data(self, user_data: pd.DataFrame):       
         user_data = self.remove_null(user_data)
         user_data = self.clean_invalid_dates(user_data, 'join_date')
         user_data = self.clean_invalid_dates(user_data, 'date_of_birth')      
         #Remove all null data
-        user_data.dropna(inplace=True) 
-        #Return cleaned user data   
+        user_data.dropna(inplace=True)
+        #Return cleaned user data          
         return user_data
      
-
     #Cleans card data removing null valus, duplicates, and changing confirmation date to specific format
     def clean_card_data(self, card_details: pd.DataFrame):
         card_details = self.remove_null(card_details)      
@@ -32,17 +48,14 @@ class DataCleaning:
         card_details.drop_duplicates(subset=['card_number'], keep='first', inplace=True)
         card_details = self.clean_invalid_dates(card_details, 'date_payment_confirmed')
         card_details.dropna(inplace=True)
-
         return card_details
 
     def clean_store_data(self, store_data:pd.DataFrame):
-        store_data.drop(columns=['index', 'lat'], inplace=True)
-        store_data.fillna('N/A', inplace=True)
+        store_data.drop(columns=['index'], inplace=True)
         store_data = self.remove_null(store_data)
         store_data = self.clean_invalid_dates(store_data, 'opening_date')
         store_data['staff_numbers'] = store_data['staff_numbers'].str.replace(r'[^0-9]', '', regex=True)
-        store_data.dropna(inplace=True)
-        
+        store_data.dropna(subset=['opening_date'], inplace=True)        
         return store_data
     
     
@@ -71,7 +84,7 @@ class DataCleaning:
         elif 'oz' in weight:
             return float(weight.replace('oz', '')) *0.0283495
         #Any data that cannot be converted will be changed to NaN
-        else:
+        else:           
             return np.nan
         
     #Cleans product data, removes null data and converts all weights to kg    
@@ -83,7 +96,7 @@ class DataCleaning:
     
     #Drop unnecesary data in the orders table
     def clean_orders_data(self, orders_data:pd.DataFrame):
-        orders_data.drop(columns=['first_name', 'last_name', '1', 'level_0', 'index'], inplace=True)
+        orders_data.drop(columns=['first_name', 'last_name', '1', 'level_0', 'index'], inplace=True)       
         return orders_data
     
     #Cleans the events data by removing invalid dates from the year, month, and day columns. Removes any null values
@@ -96,7 +109,7 @@ class DataCleaning:
         #Creates a date column to ensure remaining data contains only valid dates
         events_data['date'] = pd.to_datetime(events_data[['day', 'month', 'year']], errors='coerce')
         events_data.dropna(inplace=True)
-        events_data.reset_index(drop=True, inplace=True)
+        events_data.reset_index(drop=True, inplace=True)        
         return events_data
 
         
--- data_extraction.py
+++ data_extraction.py
@@ -17,11 +17,12 @@ class DataExtractor:
         return data
     
     #Reads data from a specific PDF and returns it as a dataframe
-    def retrieve_pdf_data():
+    def retrieve_pdf_data(self):
         file = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
         card_details_list = tabula.read_pdf(file, lattice=True, pages = 'all')
         card_details = pd.concat(card_details_list,ignore_index=True)
         return card_details
+    
     #Key used to access store data
     def api_key(self):
         return {"x-api-key" : "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
@@ -29,16 +30,16 @@ class DataExtractor:
     #Creates a list of store numbers    
     def list_store_numbers(self):
         number_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
-        response = requests.get(number_endpoint, headers=self.api_key)
+        response = requests.get(number_endpoint, headers=self.api_key())
         return response.json()['number_stores']
 
-    #Takes the list of store numbers to get the data for each store and collate it into a dataframe
+    #Takes the list of store numbers to get the data for each store and collates it into a dataframe
     def retrieve_store_data(self):
         number_of_stores = self.list_store_numbers()
         store_data_list = []       
         for store_number in range (0, number_of_stores):
                 store_endpoint = f"https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}"
-                response = requests.get(store_endpoint, headers=self.api_key)
+                response = requests.get(store_endpoint, headers=self.api_key())
                 store_data_list.append(response.json())
 
         return pd.DataFrame(store_data_list)
